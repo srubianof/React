5123f0e8728ecbaebcd38cdac3760ff5980f3764
Santiago <srubianof@icloud.com>
2021-01-05 18:07:24 -0500

    start s34

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import PrimeraApp from "./PrimeraApp";
 import './index.css'
 
+// const saludo = <h1>Hola Mundo</h1>
 
+// Referencia a div principal
 const divRoot = document.querySelector('#app');
-
+console.log(divRoot)
 // Para renderizar componentes que devuelven HTML utilizo <NOMBRE />
 ReactDOM.render(<PrimeraApp/>, divRoot)
