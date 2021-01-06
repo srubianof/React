d4393ff14933e37c744d996a3c629f7f49e9fd31
Santiago <srubianof@icloud.com>
2021-01-05 19:19:25 -0500

    click event

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -2,10 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  //handleAdd
+  const handleAdd = (e) => {
+    console.log(e);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2> {value} </h2>
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
